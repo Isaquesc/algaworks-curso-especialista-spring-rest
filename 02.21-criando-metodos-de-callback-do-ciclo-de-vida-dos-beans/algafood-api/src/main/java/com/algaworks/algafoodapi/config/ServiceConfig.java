c0c8ad5faa54cb21546ce8b1bb1d6243128fe337
Isaque Costa <isaquedeco@hotmail.com>
2023-04-20 01:32:30 -0300

    criando métodos de call-back do ciclo de vida dos @beans

---
+++ 02.21-criando-metodos-de-callback-do-ciclo-de-vida-dos-beans/algafood-api/src/main/java/com/algaworks/algafoodapi/config/ServiceConfig.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafoodapi.config;
+
+import com.algaworks.algafoodapi.service.AtivacaoClienteService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServiceConfig {
+    @Bean(initMethod = "init",destroyMethod = "destroy")
+    public AtivacaoClienteService ativacaoClienteService(){
+        return new AtivacaoClienteService();
+    }
+}
