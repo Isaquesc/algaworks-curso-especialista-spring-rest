261bba7525ab5a2d7ca839661ea87a470e0bf92e
Isaque Costa <isaquedeco@hotmail.com>
2023-05-22 23:02:40 -0300

    imprimindo profile ativo com Environment

--- 02.25-propriedades-customizadas-com-value/algafood-api/src/main/java/com/algaworks/algafoodapi/notificador/NotificadorEmail.java
+++ 02.25-propriedades-customizadas-com-value/algafood-api/src/main/java/com/algaworks/algafoodapi/notificador/NotificadorEmail.java
@@ -4,9 +4,15 @@ import com.algaworks.algafoodapi.anotacao.TipoNotificador;
 import com.algaworks.algafoodapi.enumerados.NivelUrgencia;
 import com.algaworks.algafoodapi.model.Cliente;
 import com.algaworks.algafoodapi.model.Response;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Optional;
 
 @TipoNotificador(value = NivelUrgencia.URGENTE)
 @Component
@@ -19,12 +25,18 @@ public class NotificadorEmail implements Notificador {
     @Value("${notificado.email.porta-servidor}")
     private Integer porta;
 
+    @Autowired
+    Environment env;
+
     @Override
     public void notificar(Cliente cliente, String mensagem) {
+
+        Optional<String> first = Arrays.stream(env.getActiveProfiles()).findFirst();
+        first.ifPresent(System.out::println);
+
         System.out.println("host: " + host);
         System.out.println("host: " + porta);
 
         System.out.println(new Response(cliente, "email",null, mensagem ));
     }
-
 }
