9b2b420149043a813cd7b06aabd398abf47297ca
Isaque Costa <isaquedeco@gmail.com>
2024-03-26 20:04:38 -0300

    incluindo anotação do lombook @EqualsAndHashCode

--- 03.14-implementando-padrao-repository/algafood-api/src/main/java/com/algaworks/algafoodapi/model/Cozinha.java
+++ 03.14-implementando-padrao-repository/algafood-api/src/main/java/com/algaworks/algafoodapi/model/Cozinha.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafoodapi.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,9 +11,13 @@ import static javax.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Cozinha {
+
     @Id
     @GeneratedValue(strategy = IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
+
     private String nome;
 }
--- 03.14-implementando-padrao-repository/algafood-api/src/main/java/com/algaworks/algafoodapi/model/Restaurante.java
+++ 03.14-implementando-padrao-repository/algafood-api/src/main/java/com/algaworks/algafoodapi/model/Restaurante.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafoodapi.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -9,11 +10,16 @@ import static javax.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Restaurante {
+
     @Id
     @GeneratedValue(strategy = IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
+
     private String nome;
+
     @Column(name = "taxa_frete")
     private BigDecimal taxaFrete;
 }
