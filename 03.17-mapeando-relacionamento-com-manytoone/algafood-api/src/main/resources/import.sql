cc0c6e8ecca9cf60a85d4842cd943f082d3b6990
Isaque Costa <isaquedeco@gmail.com>
2024-03-26 22:18:30 -0300

    implementando o padrao repository

---
+++ 03.17-mapeando-relacionamento-com-manytoone/algafood-api/src/main/resources/import.sql
@@ -0,0 +1,12 @@
+insert into cozinha (nome) values ('Italiana');
+insert into cozinha (nome) values ('Japonesa');
+insert into cozinha (nome) values ('Indiana');
+insert into cozinha (nome) values ('Coreana');
+insert into cozinha (nome) values ('Mexicana');
+insert into cozinha (nome) values ('Tailandesa');
+insert into cozinha (nome) values ('Africana');
+
+insert into restaurante (nome,taxa_frete, id_cozinha) values ('Thai Gourmet', 10, 1);
+insert into restaurante (nome,taxa_frete, id_cozinha) values ('Thai Delivery', 8, 2);
+insert into restaurante (nome,taxa_frete, id_cozinha) values ('Tuk Tuk Comida Indiana', 13, 3);
+insert into restaurante (nome,taxa_frete, id_cozinha) values ('Thai Gordela', 10, 7);
