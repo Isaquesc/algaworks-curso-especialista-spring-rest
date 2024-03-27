b12b7199c5b8bf9656a8d350d0c37134b2c5c6e7
Isaque Costa <isaquedeco@gmail.com>
2024-03-26 22:20:27 -0300

    adicionando anotação @JoinColumn para renomear o nome da coluna FOREIGN KEY

---
+++ 03.17-mapeando-relacionamento-com-manytoone/algafood-api/src/main/java/com/algaworks/algafoodapi/model/Cozinha.java
@@ -0,0 +1,23 @@
+package com.algaworks.algafoodapi.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Cozinha {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String nome;
+}
---
+++ 03.17-mapeando-relacionamento-com-manytoone/algafood-api/src/main/java/com/algaworks/algafoodapi/model/Restaurante.java
@@ -0,0 +1,29 @@
+package com.algaworks.algafoodapi.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Restaurante {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String nome;
+
+    @Column(name = "taxa_frete")
+    private BigDecimal taxaFrete;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cozinha")
+    private Cozinha cozinha;
+}
