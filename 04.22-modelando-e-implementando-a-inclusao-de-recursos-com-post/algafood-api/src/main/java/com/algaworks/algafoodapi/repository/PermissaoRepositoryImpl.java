6f6936dcba7f8c33f46bab181a566eb8597a1d93
Isaque Costa <isaquedeco@gmail.com>
2024-03-28 00:34:41 -0300

    modelando e implementando a inclusão de recursos com post

---
+++ 04.22-modelando-e-implementando-a-inclusao-de-recursos-com-post/algafood-api/src/main/java/com/algaworks/algafoodapi/repository/PermissaoRepositoryImpl.java
@@ -0,0 +1,40 @@
+package com.algaworks.algafoodapi.repository;
+
+import com.algaworks.algafoodapi.model.Permissao;
+import com.algaworks.algafoodapi.repository.interfaces.PermissaoRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Component
+public class PermissaoRepositoryImpl implements PermissaoRepository {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Transactional
+    @Override
+    public Permissao save(Permissao permissao) {
+        return manager.merge(permissao);
+    }
+
+    @Override
+    public Permissao findById(Long id) {
+        return manager.find(Permissao.class, id);
+    }
+
+    @Transactional
+    @Override
+    public void remove(Permissao permissao) {
+        manager.remove(findById(permissao.getId()));
+    }
+
+    @Override
+    public List<Permissao> findAll() {
+        return manager.createQuery("from Permissao", Permissao.class)
+                .getResultList();
+    }
+}
