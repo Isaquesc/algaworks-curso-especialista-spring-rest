3131c96d50b3df38cb9e82ac04a9a9cba3da1a55
Isaque Costa <isaquedeco@gmail.com>
2024-04-22 19:23:49 -0300

    Modelando e implementando a inclusão de recursos de restaurantes

--- 04.29-modelando-e-implementando-rescursos-de-restaurantes/algafood-api/src/main/java/com/algaworks/algafoodapi/api/controlles/RestauranteController.java
+++ 04.29-modelando-e-implementando-rescursos-de-restaurantes/algafood-api/src/main/java/com/algaworks/algafoodapi/api/controlles/RestauranteController.java
@@ -1,13 +1,13 @@
 package com.algaworks.algafoodapi.api.controlles;
 
+import com.algaworks.algafoodapi.domain.exception.EntidadeNaoEnconstradaException;
 import com.algaworks.algafoodapi.domain.model.Restaurante;
+import com.algaworks.algafoodapi.domain.repository.interfaces.CozinhaRepository;
 import com.algaworks.algafoodapi.domain.service.RestauranteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,7 +18,7 @@ public class RestauranteController {
     private RestauranteService restauranteservice;
 
     @Autowired
-    public RestauranteController(RestauranteService restauranteservice) {
+    public RestauranteController(RestauranteService restauranteservice, CozinhaRepository cozinhaRepository) {
         this.restauranteservice = restauranteservice;
     }
 
@@ -35,5 +35,18 @@ public class RestauranteController {
             return ResponseEntity.notFound().build();
 
         return ResponseEntity.ok(restaurante);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> save(@RequestBody Restaurante restaurante) {
+        try {
+            Restaurante save = restauranteservice.save(restaurante);
+
+            return ResponseEntity.status(HttpStatus.ACCEPTED)
+                    .body(save);
+
+        } catch (EntidadeNaoEnconstradaException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
