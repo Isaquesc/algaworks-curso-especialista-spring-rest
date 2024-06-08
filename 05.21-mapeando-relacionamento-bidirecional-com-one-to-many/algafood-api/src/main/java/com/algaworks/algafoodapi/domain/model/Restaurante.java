7269c585b392dc2a8d5b4f1e25a43af5e453ed08
Isaque Costa <isaquedeco@gmail.com>
2024-06-08 14:07:13 -0300

    mapeando relacionamento bidirecional com @OnToMany

---
+++ 05.21-mapeando-relacionamento-bidirecional-com-one-to-many/algafood-api/src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
@@ -0,0 +1,31 @@
+package com.algaworks.algafoodapi.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Restaurante {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String nome;
+
+    @Column(name = "taxa_frete", nullable = false)
+    private BigDecimal taxaFrete;
+
+    @ManyToOne
+//    @JsonIgnore
+    @JoinColumn(name = "cozinha_id")
+    private Cozinha cozinha;
+}
---
+++ 05.21-mapeando-relacionamento-bidirecional-com-one-to-many/algafood-api/src/main/java/com/algaworks/algafoodapi/domain/repository/RestauranteRepository.java
@@ -0,0 +1,35 @@
+package com.algaworks.algafoodapi.domain.repository;
+
+import com.algaworks.algafoodapi.domain.model.Restaurante;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RestauranteRepository extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQuerys, JpaSpecificationExecutor<Restaurante> {
+
+    List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+
+    List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long id);
+
+    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+    List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinhaId);
+
+    List<Restaurante> consultarPorNomeXML(String nome, @Param("id") Long cozinhaId);
+
+    Optional<Restaurante> findFirstByNomeContaining(String nome);
+
+    List<Restaurante> findTop2ByNomeContaining(String nome);
+
+    int countByCozinhaId(Long cozinhaId);
+
+    Optional<List<Restaurante>> findByCozinhaId(Long cozinhaId);
+
+}
