982cf05cf0b123c8b09f07f562c595b89d5bd9ac
Isaque Costa <isaquedeco@gmail.com>
2024-06-10 22:15:17 -0300

    alterando a estrategia de fetching para lazy e eager

---
+++ 06.13-alterando-a-estrategia-de-fetching-para-eager-loading/algafood-api/src/main/java/com/algaworks/algafoodapi/api/model/CozinhaXMLWrapper.java
@@ -0,0 +1,29 @@
+package com.algaworks.algafoodapi.api.model;
+
+import com.algaworks.algafoodapi.domain.model.Cozinha;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.util.List;
+
+/*
+ * @JacksonXmlRootElement é uma alternativa à @JsonRootName e
+ * @JacksonXmlProperty à @JsonProperty.
+ *
+ * A diferença é que as anotações iniciadas com @JacksonXml só afetam
+ * a serialização em XML. Já as anotações iniciadas com @Json
+ * afetam tanto a serialização JSON como também XML.
+ */
+
+@Data
+@JacksonXmlRootElement(localName = "cozinhas")
+public class CozinhaXMLWrapper {
+
+    @NonNull
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "cozinha")
+    private List<Cozinha> cozinhas;
+}
