c2873f62662d290631847e517962a2afece157c7
Isaque Costa <isaquedeco@gmail.com>
2024-06-23 12:59:55 -0300

    lancando excecoes customizadas anotadas com @ResponseStatus

---
+++ 08.02-lancando-excecoes-customizadas-anotadas-com-responsestatus/algafood-api/src/main/java/com/algaworks/algafoodapi/domain/exception/EntidadeEmUsoException.java
@@ -0,0 +1,11 @@
+package com.algaworks.algafoodapi.domain.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class EntidadeEmUsoException extends RuntimeException {
+    public EntidadeEmUsoException(String msg) {
+        super(msg);
+    }
+}
