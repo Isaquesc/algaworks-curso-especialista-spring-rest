f49e858da56048ead238c3c276a17500d92e4755
Isaque Costa <isaquedeco@gmail.com>
2024-07-15 20:36:18 -0300

    tratando excecoes globais com @ExceptionHandler e @ControllerAdvice

---
+++ 08.13-tratamento-excecoes-globais-com-exception-handler-e-controller-advice/algafood-api/src/main/java/com/algaworks/algafoodapi/domain/model/Produto.java
@@ -0,0 +1,33 @@
+package com.algaworks.algafoodapi.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false)
+    private BigDecimal preco;
+
+    @Column(nullable = false)
+    private Boolean ativo;
+
+    @ManyToOne
+    private Restaurante restaurante;
+}
