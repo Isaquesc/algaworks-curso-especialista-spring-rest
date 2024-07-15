24ad18e2a622b03696749b92af332da8cb843203
Isaque Costa <isaquedeco@gmail.com>
2024-07-15 20:50:54 -0300

    desafio implementando exception handler

---
+++ 08.14-desafio-implementando-exception-handler/algafood-api/src/main/java/com/algaworks/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.algaworks.algafoodapi.api.exceptionhandler;
+
+import com.algaworks.algafoodapi.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafoodapi.domain.exception.NegocioException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(EntidadeNaoEncontradaException.class)
+    public ResponseEntity<?> handlerEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
+        var problem = Problem.builder()
+                .localDateTime(LocalDateTime.now())
+                .mensagem(e.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(problem);
+    }
+
+    @ExceptionHandler(NegocioException.class)
+    public ResponseEntity<?> handlerNegocioException(NegocioException e) {
+        var problem = Problem.builder()
+                .localDateTime(LocalDateTime.now())
+                .mensagem(e.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_GATEWAY)
+                .body(problem);
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<?> handlerHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
+        var problem = Problem.builder()
+                .localDateTime(LocalDateTime.now())
+                .mensagem(e.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+                .body(problem);
+    }
+
+}
