d41858a7b49bb354606788ead4a245fa8c147fd9
Isaque Costa <isaquedeco@gmail.com>
2024-07-16 01:21:27 -0300

    desafio tratando PropertyBindingException na desserializacao

---
+++ 08.23-desafio-tratando-propertybindingexception-na-desserializacao/algafood-api/src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
@@ -0,0 +1,59 @@
+package com.algaworks.algafoodapi.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Restaurante {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String nome;
+
+    @CreationTimestamp
+    @Column(nullable = false, columnDefinition = "datetime")
+    private LocalDateTime data_cadastro;
+
+    @UpdateTimestamp
+    @Column(nullable = false, columnDefinition = "datetime")
+    private LocalDateTime data_atualizacao;
+
+    @Column(name = "taxa_frete", nullable = false)
+    private BigDecimal taxaFrete;
+
+//    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+//    @JsonIgnoreProperties("hibernateLazyInitializer")
+    @JoinColumn(name = "cozinha_id")
+    private Cozinha cozinha;
+
+    @Embedded
+    private Endereco endereco;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "restaurante")
+    private List<Produto> produtos;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "restaurante_forma_pagamento",
+            joinColumns = @JoinColumn(name = "restaurante_id"),
+            inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+    private List<FormaPagamento> formaPagamentos = new ArrayList<>();
+}
---
+++ 08.23-desafio-tratando-propertybindingexception-na-desserializacao/algafood-api/src/main/java/com/algaworks/algafoodapi/domain/service/EstadoService.java
@@ -0,0 +1,44 @@
+package com.algaworks.algafoodapi.domain.service;
+
+import com.algaworks.algafoodapi.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafoodapi.domain.exception.EstadoNaoEncontradoException;
+import com.algaworks.algafoodapi.domain.model.Estado;
+import com.algaworks.algafoodapi.domain.repository.EstadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EstadoService {
+
+
+    public static final String MSG_ESTADO_EM_USO = "Estado de código %d não pode ser removida, pois está em uso";
+
+    private EstadoRepository estadoRepository;
+
+    @Autowired
+    public EstadoService(EstadoRepository estadoRepository) {
+        this.estadoRepository = estadoRepository;
+    }
+
+    public Estado save(Estado estado) {
+        return estadoRepository.save(estado);
+    }
+
+    public void remove(Long estadoId) {
+        try {
+            estadoRepository.deleteById(estadoId);
+        } catch (EmptyResultDataAccessException e) {
+            throw new EstadoNaoEncontradoException(estadoId);
+        } catch (DataIntegrityViolationException e) {
+            throw new EntidadeEmUsoException(
+                    String.format(MSG_ESTADO_EM_USO, estadoId));
+        }
+    }
+
+    public Estado buscarOuFalhar(Long estadoId) {
+        return estadoRepository.findById(estadoId)
+                .orElseThrow(() -> new EstadoNaoEncontradoException(estadoId));
+    }
+}
