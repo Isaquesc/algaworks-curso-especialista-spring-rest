2301a0ee46247bff3c61b7b1d2b9a4e53ac786c1
Isaque Costa <isaquedeco@gmail.com>
2024-07-17 23:15:01 -0300

    Criando uma nova classe para modelar o retorno do erro com os campos validados.

---
+++ 09.04-estendendo-problem-para-adicionar-as-propriedades-com-constrains-violadas/algafood-api/src/main/java/com/algaworks/algafoodapi/domain/exception/NegocioException.java
@@ -0,0 +1,16 @@
+package com.algaworks.algafoodapi.domain.exception;
+
+public class NegocioException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+
+    public NegocioException(String message) {
+        super(message);
+    }
+
+    public NegocioException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+
+}
